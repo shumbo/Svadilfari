9659bed20633ad72bda4013ad16bfa0641f7f966
Shun <shunthedev@gmail.com>
2021-07-30 04:52:24 +0900

    Initial Commit

---
+++ Svadilfari/SvadilfariApp.swift
@@ -0,0 +1,17 @@
+//
+//  SvadilfariApp.swift
+//  Svadilfari
+//
+//  Created by Shun Kashiwa on 2021/07/30.
+//
+
+import SwiftUI
+
+@main
+struct SvadilfariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
