d59c102e26bdff5ccf6058192df60ea95269ae08
Shun <shunthedev@gmail.com>
2021-08-14 01:14:18 +0900

    add vector template

---
+++ Svadilfari/Extensions/Vector+.swift
@@ -0,0 +1,39 @@
+//
+//  CGPoint+init.swift
+//  CGPoint+init
+//
+//  Created by Shun Kashiwa on 2021/08/01.
+//
+
+import CoreGraphics
+
+extension Vector {
+    var point: CGPoint {
+        return CGPoint(x: self.x, y: self.y)
+    }
+    
+    static var Top: Self {
+        return Self.init(x: 0, y: -100)
+    }
+    static var TopRight: Self {
+        return Self.init(x: 100, y: -100)
+    }
+    static var Right: Self {
+        return Self.init(x: 100, y: 0)
+    }
+    static var BottomRight: Self {
+        return Self.init(x: 100, y: 100)
+    }
+    static var Bottom: Self {
+        return Self.init(x: 0, y: 100)
+    }
+    static var BottomLeft: Self {
+        return Self.init(x: -100, y: 100)
+    }
+    static var Left: Self {
+        return Self.init(x: -100, y: 0)
+    }
+    static var TopLeft: Self {
+        return Self.init(x: -100, y: -100)
+    }
+}
