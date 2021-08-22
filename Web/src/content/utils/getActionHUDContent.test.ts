bd673d2f79a9834594a5c3292dd73f44187447d5
Shun <shunthedev@gmail.com>
2021-08-22 21:17:32 +0900

    :recycle:

--- Web/src/content/utils/getActionHUDContent.test.ts
+++ Web/src/content/utils/getActionHUDContent.test.ts
@@ -1,16 +1,23 @@
+import { MockI18n } from "../../utils/MockI18n";
 import { getActionHUDContent } from "./getActionHUDContent";
+
+const i18n = new MockI18n();
 
 describe("getActionHUDContent", () => {
   test("javascriptRun without description", () => {
     expect(
-      getActionHUDContent({ javascriptRun: { code: "alert('hello')" } })?.title
+      getActionHUDContent({ javascriptRun: { code: "alert('hello')" } }, i18n)
+        ?.title
     ).toBe("Run JavaScript");
   });
   test("javascriptRun with description", () => {
     expect(
-      getActionHUDContent({
-        javascriptRun: { code: "alert('hello')", description: "Say Hello" },
-      })
+      getActionHUDContent(
+        {
+          javascriptRun: { code: "alert('hello')", description: "Say Hello" },
+        },
+        i18n
+      )
     ).toMatchObject({ title: "Run JavaScript", message: "Say Hello" });
   });
 });
