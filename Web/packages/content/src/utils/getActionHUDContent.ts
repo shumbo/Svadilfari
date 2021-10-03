9fe91792e02fedcdaf4556d4311b162a068f77e1
Shun <shunthedev@gmail.com>
2021-10-03 11:46:22 -0700

    implement extension part

--- Web/packages/content/src/utils/getActionHUDContent.ts
+++ Web/packages/content/src/utils/getActionHUDContent.ts
@@ -3,15 +3,17 @@ import ArrowCircleRight from "@fluentui/svg-icons/icons/arrow_circle_right_24_re
 import ArrowClockwise from "@fluentui/svg-icons/icons/arrow_clockwise_20_regular.svg";
 import ArrowCurveDownRight from "@fluentui/svg-icons/icons/arrow_curve_down_right_20_regular.svg";
 import ArrowCurveUpLeft from "@fluentui/svg-icons/icons/arrow_curve_up_left_20_regular.svg";
+import ArrowNext from "@fluentui/svg-icons/icons/arrow_next_20_regular.svg";
+import ArrowPrevious from "@fluentui/svg-icons/icons/arrow_previous_20_regular.svg";
 import Broom from "@fluentui/svg-icons/icons/broom_20_regular.svg";
 import ClipboardLink from "@fluentui/svg-icons/icons/clipboard_link_20_regular.svg";
 import Code from "@fluentui/svg-icons/icons/code_20_filled.svg";
 import TabAdd from "@fluentui/svg-icons/icons/tab_add_20_regular.svg";
 import TabDesktopCopy from "@fluentui/svg-icons/icons/tab_desktop_copy_20_regular.svg";
 import TabProhibited from "@fluentui/svg-icons/icons/tab_prohibited_24_regular.svg";
-import { Browser } from "webextension-typedef";
+import { Action, getActionCode } from "core";
 import { unreachableCase } from "ts-assert-unreachable";
-import { getActionCode, Action } from "core";
+import { Browser } from "webextension-typedef";
 
 import { HUDContent } from "../components/HUD/HUD";
 
@@ -51,6 +53,10 @@ export function getActionHUDContent(
       return { title: getTitle("scroll_top"), icon: ArrowCurveUpLeft };
     case "scrollBottom":
       return { title: getTitle("scroll_bottom"), icon: ArrowCurveDownRight };
+    case "goBackward":
+      return { title: getTitle("scroll_bottom"), icon: ArrowPrevious };
+    case "goForward":
+      return { title: getTitle("scroll_bottom"), icon: ArrowNext };
     case null:
       break;
     default:
