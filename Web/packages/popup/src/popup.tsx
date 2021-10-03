d43965c43a0c817060f8fa90a23645f019f3c7dc
Shun <shunthedev@gmail.com>
2021-10-03 13:12:14 -0700

    use system color mode on popup

--- Web/packages/popup/src/popup.tsx
+++ Web/packages/popup/src/popup.tsx
@@ -1,4 +1,4 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom";
 
@@ -9,7 +9,11 @@ import { PopupTabManagerImpl } from "./PopupTabManager";
 const root = document.getElementById("root");
 if (root) {
   ReactDOM.render(
-    <ChakraProvider>
+    <ChakraProvider
+      theme={extendTheme({
+        config: { initialColorMode: "light", useSystemColorMode: true },
+      })}
+    >
       <PopupApp
         i18n={browser.i18n}
         messenger={new PopupMessengerImpl()}
