47ee95c777ba4db1228e5aa4edbb55a9878f3818
Shun Kashiwa <shunthedev@gmail.com>
2024-09-14 17:41:27 -0700

    commit

--- Web/packages/background/build.js
+++ Web/packages/background/build.js
@@ -1,7 +1,9 @@
-const path = require("path");
-const { buildWebExtension, getExtensionRoot } = require("webextension-toolkit");
+/* eslint-env node */
+import { join } from "path";
+
+import { buildWebExtension, getExtensionRoot } from "webextension-toolkit";
 
 buildWebExtension({
   entryPoint: "./src/index.ts",
-  outfile: path.join(getExtensionRoot(), "Resources/background.js"),
+  outfile: join(getExtensionRoot(), "Resources/background.js"),
 });
